c3daf13dfe9539e31d72c5459bf93e5d8e098f02
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-11-28 22:59:10 +0300

    предварительное решение дз

---
+++ HW3Java/task1.java
@@ -0,0 +1,19 @@
+// package HW3Java;
+// // Реализовать алгоритм сортировки слиянием
+
+// public class task1 {
+//     public static void main(String[] args) {
+//         int[] arr = { 5, 4, 3, 276, 9, 24, 57, -23 };
+//         int[] vr;
+//         int[]currentSrc = arr;
+//         int[]currnetDest = new int[arr.length];
+//         int size = 1;
+//         while (size < arr.length) {
+//             for (int i = 0; i < arr.length; i += 2 * size) {
+                
+//             }
+//         }
+
+//     }
+
+// }
