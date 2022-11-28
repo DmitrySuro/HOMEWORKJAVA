01ef15e7bff8659f1c99388e7a16a1f10469f6d7
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-11-28 20:19:35 +0300

    add task2

--- HW2Java/task2.java
+++ HW2Java/task2.java
@@ -3,6 +3,7 @@ package HW2Java;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.util.Arrays;
 
 // Реализуйте алгоритм сортировки пузырьком числового массива, 
 // результат после каждой итерации запишите в лог-файл.
@@ -10,27 +11,21 @@ import java.io.FileWriter;
 public class task2 {
     public static void main(String[] args) throws Exception {
         int[] arr = { 5, 4, 3, 276, 9, 24, 57, -23 };
-        
         FileWriter log = new FileWriter("log_arr.txt");
-        FileOutputStream out = new FileOutputStream("c://temp//testFile2.txt");
-        out.write(log.getBytes());
-
-
-
         boolean flag = false;
         while (!flag) {
             flag = true;
             for (int i = 1; i < arr.length; i++) {
-                if (arr[i] > arr[i - i]) {
+                if (arr[i] < arr[i - 1]) {
                     int temp = arr[i];
                     arr[i] = arr[i - 1];
                     arr[i - 1] = temp;
                     flag = false;
-
                 }
-            log.append();
-            }    
-        }  
-        log.close();    
+            log.append(Arrays.toString(arr));
+            log.append("\n");
+            }
+        }
+        log.close();
     }
 }
