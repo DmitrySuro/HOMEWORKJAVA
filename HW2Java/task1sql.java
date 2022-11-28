98a47dc600b77e9971c19abc93e0ac02702548f1
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-11-28 20:08:19 +0300

    add task1Sql

--- HW2Java/task1sql.java
+++ HW2Java/task1sql.java
@@ -1,6 +1,5 @@
 package HW2Java;
 
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -11,22 +10,24 @@ import java.util.Scanner;
 // Если значение null, то параметр не должен попадать в запрос.
 // Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
 
-public class task1sql {/**
- * @param args
- * @throws FileNotFoundException
- */
-public static void main(String[] args) throws Exception {
-    
-    FileReader rd = new FileReader("C:/Users/Пользователь/Desktop/HomeWorkJava/HW2Java/task1.json");
-    Scanner st = new Scanner(rd);
-    String s = st.nextLine();
-    rd.close();
-    System.out.println(s);
-    String s = s.split(',',1);
-    System.out.println(s);    
+public class task1sql {
 
+    public static void main(String[] args) throws Exception {
 
+        FileReader rd = new FileReader("C:/Users/Пользователь/Desktop/HomeWorkJava/HW2Java/task1.json");
+        Scanner st = new Scanner(rd);
+        String s = st.nextLine();
+        rd.close();
+        s = s.substring(1, s.length() - 1);
+        s = s.replaceAll("\"", "");
+        System.out.println(s);
+        String[] words = s.split(",");
+        for (int i = 0; i < words.length; i++) {
+            String[] temp = words[i].split(": ");
+            if (!temp[1].equals("null")) {
+                System.out.println(temp[0] + " and " + temp[1]);
+            }
+        }
+    }
 
-}
-    
 }
