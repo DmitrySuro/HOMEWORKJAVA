c87a95c511bc9403442d92549fbcd6a978fe0cd8
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-11-30 21:53:49 +0300

    add Task1 for hw3

--- HW3Java/task1.java
+++ HW3Java/task1.java
@@ -1,19 +1,42 @@
-// package HW3Java;
-// // Реализовать алгоритм сортировки слиянием
+package HW3Java;
+// Реализовать алгоритм сортировки слиянием
 
-// public class task1 {
-//     public static void main(String[] args) {
-//         int[] arr = { 5, 4, 3, 276, 9, 24, 57, -23 };
-//         int[] vr;
-//         int[]currentSrc = arr;
-//         int[]currnetDest = new int[arr.length];
-//         int size = 1;
-//         while (size < arr.length) {
-//             for (int i = 0; i < arr.length; i += 2 * size) {
-                
-//             }
-//         }
+import java.util.Arrays;
 
-//     }
+public class task1 {
+    public void SortArray(int[] arr, int firstPos, int lastPos){
+        
+        int middleArray = firstPos + (firstPos - lastPos) /2;
+        SortArray(arr, firstPos, middleArray);
+        SortArray(arr, middleArray + 1, lastPos);
+        int[] buf = Arrays.copyOf(arr, arr.length);
+        for (int k = 0; k < lastPos; k++) {
+            buf[k] = arr[k];
+        }
+        int i = firstPos;
+        int j = middleArray + 1;
+        for (int k = firstPos; k < buf.length; k++) {
+            if (i > middleArray) {
+                arr[k] = buf[j];
+            }
+            else if (j > lastPos) {
+                arr[k] = buf[i];
+                i++;
+            }
+            else if (buf[j] < buf[i]) {
+                arr[k] = buf[j];
+                j++;
+            }
+            else {
+                arr[k] = buf[i];
+                i++;
+            }
+        }
 
-// }
+        int[] array = {1,3,2,5,10,4,0};
+        int firstPosition = 0;
+        int lastPosition = arr.length -1;
+        SortArray(array, firstPosition, lastPosition);
+        System.out.println(Arrays.toString(array));
+    }
+}
