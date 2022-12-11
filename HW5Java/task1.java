c27fc3a22f9e25a6e57d100450b8ebd78c872060
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-12-11 17:27:20 +0300

    add task

--- HW5Java/task1.java
+++ HW5Java/task1.java
@@ -1,9 +1,21 @@
 package HW5Java;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 // Реализуйте структуру телефонной книги
 //  с помощью HashMap, учитывая, что 1 человек 
 //  может иметь несколько телефонов.
-public class task1 {public static void main(String[] args) {
-    
-}
-    
+public class task1 {
+    public static void main(String[] args) {
+        HashMap<String, List<Integer>> telephone_book = new HashMap<>();
+        List<Integer> telephone_list = new ArrayList<>();
+        telephone_list.add(456);
+        telephone_list.add(4343);
+        telephone_book.put("Ivan Ivanov", telephone_list);
+        System.out.println(telephone_book);
+        
+    }
 }
